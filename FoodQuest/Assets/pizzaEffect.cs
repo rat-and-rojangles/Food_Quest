33fb954c9f569fc3837a5eb84008e7e36958d9c4
Daniel Dakev <ddakev@gmail.com>
2015-11-07 19:43:35 -0500

    PizzaTrigger
    Pizza Trigger object is now working

---
+++ FoodQuest/Assets/pizzaEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pizzaEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "CharacterRobotBoy")
+            Destroy(gameObject);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
