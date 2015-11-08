88dbbc566d7fc9616a5d9db7b35751834d307ce1
Daniel Dakev <ddakev@gmail.com>
2015-11-08 00:23:02 -0500

    Update on checkpoints
    First update on the checkpoints mod.

--- FoodQuest/Assets/Standard Assets/2D/Scripts/FallOutOfBounds.cs
+++ FoodQuest/Assets/Standard Assets/2D/Scripts/FallOutOfBounds.cs
@@ -14,15 +14,15 @@ public class FallOutOfBounds : MonoBehaviour {
         if(other.gameObject.name=="CharacterRobotBoy")
         {
             other.gameObject.transform.position = other.gameObject.GetComponent<PlatformerCharacter2D>().m_LastCheckpoint.transform.position;
-            /*foreach(GameObject go in other.gameObject.GetComponent<PlatformerCharacter2D>().usedItems)
+            foreach(GameObject go in other.gameObject.GetComponent<PlatformerCharacter2D>().usedItems)
             {
                 go.GetComponent<Renderer>().enabled = true;
-            }*/
+            }
             other.gameObject.GetComponent<PlatformerCharacter2D>().usedItems.Clear();
         }
         else
         {
-            Destroy(other.gameObject);
+            other.gameObject.GetComponent<Renderer>().enabled = false;
         }
     }
 	
