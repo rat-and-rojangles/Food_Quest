a11e73a8c640835f0fb2d023516cd1f6a9fee833
Maggie Dakeva <mpd13@duke.edu>
2015-11-07 21:07:50 -0500

    Increase speed when character hits trigger

---
+++ FoodQuest/Assets/accelerateEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class accelerateEffect : MonoBehaviour {
+
+    public float m_NewMaxSpeed = 15f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "CharacterRobotBoy")
+        {
+            other.gameObject.GetComponent<PlatformerCharacter2D>().m_MaxSpeed = m_NewMaxSpeed;
+            Destroy(gameObject);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
