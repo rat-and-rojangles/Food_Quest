dd6b6c71a6972207a029eb7d4872e5fa1c2f5685
Collin Irwin <irwincollin+github@gmail.com>
2015-11-07 20:08:18 -0500

    Pizza effect!
    Not particularly clean implementation of pizza effect... but this is a
    hackathon. And this is our first time with unity!
    
    Pizza effect uses a few constructor variables and modifies directly the
    jump variables of the player script.

--- FoodQuest/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ FoodQuest/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -7,7 +7,7 @@ namespace UnityStandardAssets._2D
     [RequireComponent(typeof (PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
-        private PlatformerCharacter2D m_Character;
+        public PlatformerCharacter2D m_Character;
         private bool m_Jump;
 
 
@@ -30,7 +30,8 @@ namespace UnityStandardAssets._2D
         private void FixedUpdate()
         {
             // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            //bool crouch = Input.GetKey(KeyCode.LeftControl);
+            bool crouch = false;
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
