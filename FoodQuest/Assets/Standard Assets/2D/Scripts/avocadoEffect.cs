db5b4464a1b286a6703888e0b899cbf00424152d
Maggie Dakeva <mpd13@duke.edu>
2015-11-08 02:38:18 -0500

    Added textboxes for food items

--- FoodQuest/Assets/Standard Assets/2D/Scripts/avocadoEffect.cs
+++ FoodQuest/Assets/Standard Assets/2D/Scripts/avocadoEffect.cs
@@ -2,21 +2,26 @@
 using System.Collections;
 using UnityStandardAssets._2D;
 
-public class pizzaEffect : MonoBehaviour {
-    [SerializeField] public float m_newJumpForce = 600f;
-    [SerializeField] public float m_EffectTime = 30.0f;
+public class avocadoEffect : MonoBehaviour
+{
+    [SerializeField]
+    public float m_newJumpForce = 600f;
+    [SerializeField]
+    public float m_EffectTime = 30.0f;
 
     public bool toggleGUI = false;
 
     private PlatformerCharacter2D pc;
     // Use this for initialization
-    void Start () {
-	    
-	}
+    void Start()
+    {
+
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "CharacterRobotBoy") {
+        if (other.gameObject.name == "CharacterRobotBoy")
+        {
             pc = other.gameObject.GetComponent<PlatformerCharacter2D>();
             other.gameObject.GetComponent<PlatformerCharacter2D>().usedItems.Add(gameObject);
             pc.PizzaEffect(m_newJumpForce, m_EffectTime);
@@ -37,8 +42,8 @@ public class pizzaEffect : MonoBehaviour {
     {
         if (toggleGUI == true)
 
-            GUI.Box(new Rect(10, 300, 800, 50), "The cheese on pizza is high in saturated fat, which increases your risk for high cholesterol and heart disease.");
-            GUI.skin.box.normal.textColor = Color.white;
+            GUI.Box(new Rect(10, 300, 800, 50), "Avocado is loaded with heart-healthy monounsaturated fatty acids.");
+        GUI.skin.box.normal.textColor = Color.white;
     }
 
     // Update is called once per frame
--- FoodQuest/Assets/Standard Assets/2D/Scripts/steakEffect.cs
+++ FoodQuest/Assets/Standard Assets/2D/Scripts/steakEffect.cs
@@ -7,6 +7,8 @@ public class steakEffect : MonoBehaviour
     [SerializeField]
     public float m_EffectTime = 15.0f;
 
+    public bool toggleGUI = false;
+
     private PlatformerCharacter2D pc;
     // Use this for initialization
     void Start()
@@ -22,6 +24,28 @@ public class steakEffect : MonoBehaviour
             other.gameObject.GetComponent<PlatformerCharacter2D>().usedItems.Add(gameObject);
             pc.SteakEffect(m_EffectTime);
             gameObject.GetComponent<Renderer>().enabled = false;
+
+            StartCoroutine("wait");
         }
+    }
+    IEnumerator wait()
+    {
+        toggleGUI = true;
+        yield return new WaitForSeconds(3);
+        toggleGUI = false;
+    }
+
+    void OnGUI()
+    {
+        if (toggleGUI == true)
+
+            GUI.Box(new Rect(10, 300, 800, 50), "Red meat provides protein, which helps build bones and muscle strength.");
+            GUI.skin.box.normal.textColor = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 }
